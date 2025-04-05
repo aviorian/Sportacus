5ad34512c5c914ee2e7d93508400069d83985155
HasanBerberkayar <119157476+HasanBerberkayar@users.noreply.github.com>
2025-04-05 12:04:19 +0300

    BackendSetup

---
+++ Sportacuss/backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import { connectDB } from "./db/connetDB.js";
+import authRoutes from "./routes/authRoute.js";
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World! 123');   
+    });
+
+app.use("/api/auth", authRoutes);
+
+app.listen(3000, () => {
+    connectDB();
+    console.log('Server is running on port 3000');
+    });
