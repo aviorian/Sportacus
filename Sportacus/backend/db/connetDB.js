921d2acd7c2f17c5f2a38014d573331a743b3244
Fatih Altınışık <fatih_altinisik@outlook.com>
2025-04-05 15:51:59 +0300

    connectdb.js file updated

--- Sportacus/backend/db/connetDB.js
+++ Sportacus/backend/db/connetDB.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const connectDB = async () => {
-    try {
-        console.log("mongore uri", process.env.MONGO_URI);
-        const conn = await mongoose.connect(process.env.MONGO_URI);        
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error("MongoDB connection error:", error.message);
-        process.exit(1); // Exit the process with failure
-    }
-}
+  try {
+    console.log("mongore uri", process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1); // Exit the process with failure
+  }
+};
